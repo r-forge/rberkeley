7920942eedfb0ac09a0a3050d700a8d6a36ca0d0
jryan (Mr ) <jeff.a.ryan@gmail.com>
2009-07-15 18:33:06 +0000

    o   new R level wrappers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rberkeley@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/db.R
+++ pkg/R/db.R
@@ -1,27 +1,96 @@
-db_create <- function() {
-  .Call("rberkeley_db_create")
+db_create <- function(dbenv=NULL) {
+  .Call("rberkeley_db_create", dbenv)
 }
 
-DB_open <- function(dbh) {
-  .Call("rberkeley_db_open", dbh)
+db_open <- function(dbh, file="access.db", flags=0L) {
+  if(check_pointer(dbh))
+    .Call("rberkeley_db_open", dbh, NULL, file, flags)
 }
 
-DB_put <- function(db, key, data)
+db_close <- function(dbh) {
+  if(check_pointer(dbh))
+  .Call("rberkeley_db_close", dbh)
+}
+
+db_put <- function(dbh, key, data)
 {
   if(!is.raw(key))
     key <- serialize(key, NULL)
   if(!is.raw(data))
     data <- serialize(data, NULL)
 
-  .Call("rberkeley_db_put", db, key, data)
+  if(check_pointer(dbh))
+  .Call("rberkeley_db_put", dbh, key, data)
 }
 
-DB_get <- function(db, key)
+db_get <- function(dbh, key)
 {
   if(!is.raw(key))
     key <- serialize(key, NULL)
 
-  .Call("rberkeley_db_get", db, key)
+  if(check_pointer(dbh))
+  .Call("rberkeley_db_get", dbh, key)
 }
 
+db_get_byteswapped <- function(dbh)
+{
+  if(check_pointer(dbh))
+    .Call("rberkeley_db_get_byteswapped", dbh)
+}
 
+db_set_cachesize <- function(dbh, gbytes, bytes, ncache)
+{
+  .Call("rberkeley_db_set_cachesize", 
+        dbh, 
+        as.integer(gbytes),
+        as.integer(bytes),
+        as.integer(ncache))
+}
+
+db_get_cachesize <- function(dbh)
+{
+  cachesize <- .Call("rberkeley_db_get_cachesize", dbh)
+  names(cachesize) <- c("gbytes","bytes","ncache")
+  cachesize
+}
+
+db_set_pagesize <- function(dbh, pagesize)
+{
+  .Call("rberkeley_db_set_pagesize", dbh, as.integer(pagesize))
+}
+
+db_get_pagesize <- function(dbh)
+{
+  pagesize <- .Call("rberkeley_db_get_pagesize", dbh)
+  pagesize
+}
+
+db_set_encrypt <- function(dbh, passwd, flags)
+{
+  if(missing(flags))
+    flags = mkFlags(DB_ENCRYPT_AES)
+  
+  .Call("rberkeley_db_set_encrypt", dbh, passwd, flags)
+}
+ 
+db_set_lorder <- function(dbh, lorder)
+{
+  lorder <- as.integer(lorder)
+  if(lorder != 1234L || lorder != 4321L)
+    stop("incorrect 'lorder' value")
+
+  .Call("rberkeley_db_set_lorder", dbh, lorder)
+}
+
+db_get_lorder <- function(dbh)
+{
+  lorder <- .Call("rberkeley_db_get_lorder", dbh)
+  if(lorder == 1234L)
+   return("little.endian")
+  if(lorder == 4321L)
+   return("big.endian")
+}
+
+db_env_create <- function(flags) {
+  .Call("rberkeley_db_env_create", flags)
+}
